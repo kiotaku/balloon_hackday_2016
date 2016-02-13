554031801f7408f142367722bb5ab98f15f890c6
kazuki <kazukunn1013@yahoo.co.jp>
2016-02-13 11:24:27 +0900

    Preset作りました

---
+++ AutoRingtone/app/src/main/java/kei/balloon/autoringtone/RingtoneChanger.java
@@ -0,0 +1,26 @@
+package kei.balloon.autoringtone;
+
+/**
+ * Created by kazuki on 2016/02/13.
+ */
+public class RingtoneChanger {
+
+    RingtonePreset activePreset;
+    LatLng currentLocation;
+
+    public RingtoneChanger(){
+
+    }
+
+    public void setActiveRingtone(){
+        //現在地と各Presetの距離比較
+
+        //最小距離をセット
+        //範囲内なければ，デフォルト
+    }
+
+    public void setLocation(LatLng l){
+        currentLocation = l;
+    }
+
+}
