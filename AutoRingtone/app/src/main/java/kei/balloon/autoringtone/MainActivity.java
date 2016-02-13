0ae40f1dc1f949fdf6686eeb23b9f1b26ae531b5
kiotaku <kkutalol@gmail.com>
2016-02-13 13:52:00 +0900

    tmp

--- AutoRingtone/app/src/main/java/kei/balloon/autoringtone/MainActivity.java
+++ AutoRingtone/app/src/main/java/kei/balloon/autoringtone/MainActivity.java
@@ -1,8 +1,9 @@
 package kei.balloon.autoringtone;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -20,12 +21,14 @@ public class MainActivity extends AppCompatActivity{
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final Context context = this;
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(context, IconSelectActivity.class);
+                startActivity(intent);
             }
         });
 
